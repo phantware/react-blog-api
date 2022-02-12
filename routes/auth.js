c3c6a4f297614d5c3c821840ec4dcd352a4e2c9f
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-13 00:18:58 +0100

    password hashed

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,17 @@
 const router = require('express').Router()
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 //Register
 router.post('/register', async (req, res) => {
   const { username, email, password } = req.body
   try {
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
     })
     const user = await newUser.save()
     return res.status(200).json(user)
