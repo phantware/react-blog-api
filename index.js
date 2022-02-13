7a5d4b6d890b850f86d9b2c5ebcce99997be6833
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-13 11:35:07 +0100

    Update User router creater

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const multer = require('multer')
 const authRoute = require('./routes/auth')
+const userRoute = require('./routes/users')
 
 dotenv.config()
 
@@ -15,6 +16,7 @@ mongoose
   })
 app.use(express.json())
 app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
 
 app.get('/', (req, res) => {
   return res.status(200).json({ msg: 'Welcome to homepage' })
