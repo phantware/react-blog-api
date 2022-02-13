8c5e1f33a0aa0ddc7b4762bafb5191631c8ac7f6
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-13 02:11:18 +0100

    Login Route created

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@ const router = require('express').Router()
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 
+router.get('/', (req, res) => {
+  res.status(200).json({ msg: 'you are welcome to auth page' })
+})
+
 //Register
 router.post('/register', async (req, res) => {
   const { username, email, password } = req.body
@@ -20,5 +24,22 @@ router.post('/register', async (req, res) => {
   }
 })
 //Login
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username })
+    if (!user) {
+      return res.status(400).json('Wrong Credentials')
+    }
+    const validate = await bcrypt.compare(req.body.password, user.password)
+    if (!validate) {
+      return res.status(400).json('Wrong Credentials')
+    }
+    const { password, ...others } = user._doc
+    return res.status(200).json(others)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json(error)
+  }
+})
 
 module.exports = router
