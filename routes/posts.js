0fa58dfc5040d2b974203b30cd93b52b1d413966
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-13 18:47:33 +0100

    create post route updated

--- routes/posts.js
+++ routes/posts.js
@@ -7,7 +7,7 @@ const Post = require('../models/Post')
 router.post('/', async (req, res) => {
   const newPost = new Post(req.body)
   try {
-    const savedPost = newPost.save()
+    const savedPost = await newPost.save()
     return res.status(200).json(savedPost)
   } catch (error) {
     return res.status(500).json(error)
