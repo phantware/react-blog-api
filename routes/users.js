e450b36abc181cb654a7b92be81d144fda276ef3
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-13 12:18:48 +0100

    Delet user and user post logic created

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const Post = require('../models/Post')
 
 //Update
 router.put('/:id', async (req, res) => {
@@ -26,4 +27,25 @@ router.put('/:id', async (req, res) => {
   }
 })
 
+//Delet User
+
+router.delete('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id)
+      try {
+        await Post.deleteMany({ username: user.username })
+        await User.findByIdAndDelete(req.params.id)
+        return res.status(200).json('User has been deleted successfully')
+      } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+      }
+    } catch (error) {
+      return res.status(404).json('User not found')
+    }
+  } else {
+    return res.status(401).json('You can only delete your account')
+  }
+})
 module.exports = router
