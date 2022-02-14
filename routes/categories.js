2dfcc11bf92066fdceee3fe72846fc6425139fff
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-14 09:55:15 +0100

    Get categories routes created

--- routes/categories.js
+++ routes/categories.js
@@ -12,4 +12,14 @@ router.post('/', async (req, res) => {
   }
 })
 
+//Get Categories
+router.get('/', async (req, res) => {
+  try {
+    const cats = await Category.find()
+    return res.status(200).json(cats)
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
 module.exports = router
