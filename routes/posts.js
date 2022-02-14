26c64a634370edd282ceb7233627d5f4669d49c5
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-14 08:43:46 +0100

    Delete Post route created

--- routes/posts.js
+++ routes/posts.js
@@ -40,4 +40,24 @@ router.put('/:id', async (req, res) => {
   }
 })
 
+// Delete Update
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (post.username === req.body.username) {
+      try {
+        await post.delete()
+        return res.status(200).json('Post has been deleted... ')
+      } catch (error) {
+        return res.status(500).json(error)
+      }
+    } else {
+      return res.status(401).json('You can delete only your post')
+    }
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
 module.exports = router
