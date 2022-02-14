22d3da93aef13035364dec04cff5073db483099b
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-14 08:36:38 +0100

    update post route created

--- routes/posts.js
+++ routes/posts.js
@@ -14,4 +14,30 @@ router.post('/', async (req, res) => {
   }
 })
 
+// Update Post
+
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        )
+        return res.status(200).json(updatedPost)
+      } catch (error) {
+        return res.status(500).json(error)
+      }
+    } else {
+      return res.status(401).json('You can update only your post')
+    }
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
 module.exports = router
